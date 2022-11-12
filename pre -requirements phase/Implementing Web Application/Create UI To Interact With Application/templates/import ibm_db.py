6bba43fced60d519175e852383caafa16781ce45
lokii0806 <113345192+lokii0806@users.noreply.github.com>
2022-11-12 14:47:52 +0530

    Delete pre -requirements phase/Implementing Web Application/Create UI To Interact With Application directory

--- pre -requirements phase/Implementing Web Application/Create UI To Interact With Application/templates/import ibm_db.py
+++
@@ -1,4 +0,0 @@
-import ibm_db
-conn =ibm_db.connect("DATABASE=bludb;HOSTNAME=2d46b6b4-cbf6-40eb-bbce-6251e6ba0300.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=32328;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=gmz37043;PWD=5eCfk6YOpNcZhK1H",'','')
-print(conn)
-print("connection successful...")
